7ab0acecb3bbfef251eb224816cad64af8b87e55
Carolina <carito940905@gmail.com>
2025-10-30 20:39:14 -0500

    first commit

---
+++ src/main/java/com/josemiel/nicho_nativo_romeral/NichoNativoRomeralApplication.java
@@ -0,0 +1,13 @@
+package com.josemiel.nicho_nativo_romeral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NichoNativoRomeralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NichoNativoRomeralApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josemiel/nicho_nativo_romeral/NichoNativoRomeralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josemiel.nicho_nativo_romeral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NichoNativoRomeralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
