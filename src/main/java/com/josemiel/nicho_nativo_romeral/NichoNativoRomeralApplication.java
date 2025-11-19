dc75b9c52ee04ba98a72d271bb44a22bfe0ecc56
Carolina <carito940905@gmail.com>
2025-11-18 23:22:13 -0500

    Feat config main class with JwtProperties

--- src/main/java/com/josemiel/nicho_nativo_romeral/NichoNativoRomeralApplication.java
+++ src/main/java/com/josemiel/nicho_nativo_romeral/NichoNativoRomeralApplication.java
@@ -2,7 +2,10 @@ package com.josemiel.nicho_nativo_romeral;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import com.josemiel.nicho_nativo_romeral.infrastructure.config.JwtProperties;
 
+@EnableConfigurationProperties(JwtProperties.class)
 @SpringBootApplication
 public class NichoNativoRomeralApplication {
 
